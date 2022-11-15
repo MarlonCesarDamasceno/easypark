7a186be9fa560cd97b21dce7d82c160ae0c52448
Marlon Cesar Damasceno <marlon.cdamasceno@gmail.com>
2022-11-15 18:31:36 -0300

    Realizando as configurações iniciais do projeto.

---
+++ EasyPark/Configuration/ProjectConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using EasyPark.EasyPark.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyPark.Configuration
+{
+    public static class ProjectConfiguration
+    {
+
+        public static IServiceCollection ConfigurarDependencias(this IServiceCollection service, IConfiguration configuration)
+        {
+            //Configure base de dados
+            service.AddDbContext<EasyParkContext>(options => options.UseSqlServer(RecuperaConnectionString(configuration)));
+            return service;
+        }
+
+
+
+        public static string RecuperaConnectionString(IConfiguration configuration)
+        {
+            return configuration.GetSection("ConfigDb:ConnectionString").Value;
+        }
+    }
+}
